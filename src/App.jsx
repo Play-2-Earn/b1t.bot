61c45aaf3b2f6079472e6c95390e1feee90307c1
Qadir Killedar <qadirk2003@gmail.com>
2025-01-26 14:10:07 +0530

    Path fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import AppRoutes from "./routes/approutes";
+import AppRoutes from "./routes/AppRoutes";
 
 const App = () => (
   <Router>
